d12adf7381c438376ef8846943e91c4118b0a663
Avijit <Avijit009>
2024-01-15 21:10:03 +0600

    Sixth Commit

---
+++ src/components/body/MenuItem.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Card, CardImg, CardImgOverlay, CardTitle, CardText } from "reactstrap";
+
+const MenuItem = ({ dish }) => {
+  return (
+    <div>
+      <Card inverse style={
+        {
+            padding: 10,
+            margin: 5,
+        }
+      }>
+        <CardImg
+          alt={dish.name}
+          src={dish.image}
+          style={{
+            height: 270,
+            opacity:0.60,
+          }}
+          width="100%"
+        />
+        <CardImgOverlay>
+
+          <CardTitle style={
+            {
+                fontSize:25,
+                fontWeight: 'bold',
+                color: '#000',
+                
+            }
+          } tag="h5">{dish.name}</CardTitle>
+          
+        </CardImgOverlay>
+      </Card>
+    </div>
+  );
+};
+
+export default MenuItem;
