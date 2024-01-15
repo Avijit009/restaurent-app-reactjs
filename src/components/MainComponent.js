c620cd2909be640c26a7ddf99a9a74e601060956
Avijit <Avijit009>
2024-01-15 19:49:17 +0600

    Fourth Commit

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import HeaderComponent from './header/HeaderComponent';
+import BodyComponent from './body/BodyComponent';
+import FooterComponent from './footer/FooterComponent';
 
 const MainComponent = () => {
   return (
-    <h1>MainComponent</h1>
+    <div>
+        <HeaderComponent />
+        <BodyComponent />
+        <FooterComponent />
+    </div>
+
   )
 }
 
