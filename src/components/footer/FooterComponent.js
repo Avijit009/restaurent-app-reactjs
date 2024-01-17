77911425e92f92a7ebca5071ad61bc64389cc1cd
Avijit <Avijit009>
2024-01-17 20:35:59 +0600

    Added footer

--- src/components/footer/FooterComponent.js
+++ src/components/footer/FooterComponent.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const FooterComponent = () => {
   return (
-    <div>FooterComponent</div>
+    <div className='footer'>
+
+      <div className='container'>
+
+        <div className='row justify-content-center'>
+          <div className='col-auto'>
+            <p> Fancy Restaurent &#169; 2024 </p>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
