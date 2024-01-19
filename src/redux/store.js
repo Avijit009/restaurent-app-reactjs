d2eb66aedb0f7df6e920fe7590e41f6d5fc0b8ce
Avijit <Avijit009>
2024-01-19 19:28:40 +0600

    Added Middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,14 @@
-import { createStore } from 'redux';
-import { Reducer } from "./reducer";
+// import { createStore } from 'redux';
+// import { Reducer } from "./reducer";
 
-const myStore = createStore(Reducer);
+// const myStore = createStore(Reducer);
 
-export default myStore;
+// export default myStore;
+
+import { createStore, applyMiddleware } from 'redux';
+import { Reducer } from './reducer';
+import logger from 'redux-logger';
+
+const myStore = createStore(Reducer, applyMiddleware(logger));
+
+export default myStore; 
